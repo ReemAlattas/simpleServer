d79d1a287a6a2a177b9a1b64d10fcd22d0a5c7eb
ReemAlattas <ralattas@live.com>
2016-10-25 14:15:12 -0400

    fix errors

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const url = require('url');
-const path = reqire('path');
+const path = require('path');
 const fs = require('fs');
 
 const mimeTypes = {
@@ -28,16 +28,18 @@ http.createServer(function(req, res){
 	}
 
 	if (stats.isFile()) {
-		var mimeType - mimeTypes[path.extname(fileName).split(".").reverse()[0]];
+		var mimeType = mimeTypes[path.extname(fileName).split(".").reverse()[0]];
 		res.writeHead(200, {'Content-type': mimeType});
 
 		var fileStream = fs.createReadStream(fileName);
 		fileStream.pipe(res);
+
 	} else if(stats.isDirectory()) {
 		res.writeHead(302, {
 			'Location': 'index.html'
 		});
 		res.end();
+
 	} else {
 		res.writeHead(500, {'Content-type': 'text/plain'});
 		res.write('500 Internal Error\n');
